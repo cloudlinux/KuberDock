450b119a256519221022605004450a1613c66830
Mystic-Mirage <mm@m10e.net>
2015-03-07 00:42:47 +0200

    Add fluentd image sources

---
+++ fluentd/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z $NODENAME ]
+then
+  NODENAME=$HOSTNAME
+fi
+
+if [ -z $ES_HOST ]
+then
+  ES_HOST=elasticsearch
+fi
+
+sed -e s/@NODENAME@/$NODENAME/g -e s/@ES_HOST@/$ES_HOST/g fluentd.conf.template > fluentd.conf
+
+exec /usr/sbin/td-agent -c fluentd.conf > /var/log/fluentd.log
