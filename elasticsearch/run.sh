40fc76dd9a17372e5eea0683fdd43620efda11df
Mystic-Mirage <mm@m10e.net>
2015-03-27 12:43:46 +0200

    Add ETCD_HOST evnironment variable

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -2,7 +2,13 @@
 
 cp /elasticsearch/config/elasticsearch.yml.template /elasticsearch/config/elasticsearch.yml
 
-ES_HOSTS=$(python3 -c "import json, requests; print([ str(x['key'].rpartition('/')[2]) for x in json.loads(requests.get('http://etcd.kuberdock:4001/v2/keys/registry/nodes').text)['node']['nodes'] ])")
+if [ -z $ETCD_HOST ]
+then
+  ETCD_HOST=etcd
+fi
+
+
+ES_HOSTS=$(python3 -c "import json, requests; print([ str(x['key'].rpartition('/')[2]) for x in json.loads(requests.get('http://$ETCD_HOST:4001/v2/keys/registry/nodes').text)['node']['nodes'] ])")
 
 if [ ! -z "$ES_HOSTS" -a "$ES_HOSTS" != "[]" ]
 then
