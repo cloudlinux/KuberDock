cbc56e6d05bc8f8d0672a3f31cb6637ab53c3e0a
Mystic-Mirage <mm@m10e.net>
2016-01-15 02:00:05 +0200

    Move make_elastic_config.py to main KuberDock git repo

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -4,6 +4,6 @@ BASE='/elasticsearch'
 
 cp $BASE/config/elasticsearch.yml.template $BASE/config/elasticsearch.yml
 
-python3 $BASE/make_elastic_config.py >> $BASE/config/elasticsearch.yml
+python3 $BASE/data/make_elastic_config.py >> $BASE/config/elasticsearch.yml
 
 exec $BASE/bin/elasticsearch
