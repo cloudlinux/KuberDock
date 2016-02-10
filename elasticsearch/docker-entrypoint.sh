3406756b9529e98d635cc01630ba8d3667ee5edd
Aleksandr Kuznetsov <aku.ru.kz@gmail.com>
2016-02-10 17:35:42 +0300

    Elasticsearch upgraded to version 2.2

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -e
 
 BASE='/elasticsearch'
+
+chown -R elasticsearch "$BASE" 2> /dev/null || true
 
 cp $BASE/config/elasticsearch.yml.template $BASE/config/elasticsearch.yml
 
 python3 $BASE/data/make_elastic_config.py >> $BASE/config/elasticsearch.yml
 
-exec $BASE/bin/elasticsearch
+
+if [ "$1" = 'elasticsearch' ]; then
+
+    shift
+    exec gosu elasticsearch $BASE/bin/elasticsearch "$@"
+
+fi
+
+exec "$@"
+
